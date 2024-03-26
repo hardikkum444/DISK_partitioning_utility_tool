2d7729461fe28975c74ed300d1ae57853ccb2e61
man44 <114590291+hardikkum444@users.noreply.github.com>
2024-03-26 19:01:38 +0530

    cleaner??

--- diskutil.py
+++ diskutil.py
@@ -27,10 +27,26 @@ def listing():
         print(f"Error: {e}")
 
 def what():
-    print("\n1>format the disk\n2>create new partition\n3>unmount disk\n4>mount disk")
+    print("\n1>format the disk\n2>create new partition and format\n3>unmount disk\n4>mount disk")
     choice = input("please choose an option: ")
     
     if(choice == "1"):
+        try:
+              partition_no = input("partition no: ")
+              file_type = input("file system type: ")
+              new_name = input("new name: ")
+              output = subprocess.run(["sudo", f"mkfs.{file_type}","-n",f"{new_name}",f"/dev/sda{partition_no}"])
+              if output.returncode==0:
+                print(f"{GREEN}Successfuly formated to {file_type}{RESET}")
+              else:
+                print(f"{RED}Unsuccessful{RESET}")
+
+        except subprocess.CalledProcessError as e:
+              print(f"{RED}Unsuccessful{RESET}")
+
+
+
+    if(choice == "2"):
         print("make sure you have unmounted your disk first by chosing option 3")
         
         sudo = getpass.getpass("please enter your root pass: ")
@@ -47,8 +63,26 @@ def what():
         print(f"{GREEN}format completed, ready to go{RESET}")
         print(f"{GREEN}now mounting to /mnt{RESET}")
 
-        subprocess.run(["sudo","mount",f"/dev/sda{part_no}","/media/man44"])
-        print(f"{GREEN}disk has been mounted{RESET}")
+        # subprocess.run(["sudo","mount",f"/dev/sda{part_no}","/media/man44"])
+        # print(f"{GREEN}disk has been mounted{RESET}")
+
+        try:
+            partition_no = input("partition no: ")
+            user_name = input("Username: ")
+            print(f"Where to mount:\n1>/media/{user_name}\n2>/mnt")
+            ch = input("choose: ")
+            if(ch=='1'):
+                loc = f"/media/{user_name}"
+            else:
+                loc = "/mnt"
+            output = subprocess.run(["sudo","mount",f"/dev/sda{partition_no}",f"{loc}"])
+            if output.returncode==0:
+                print(f"{GREEN}Successfuly mounted{RESET}")
+            else:
+                print(f"{GREEN}Unsuccessful{RESET}")
+
+        except subprocess.CalledProcessError as e:
+            print(f"{RED}Unsuccessful{RESET}")
 
     if(choice == "3"):
         
@@ -62,6 +96,27 @@ def what():
         
         except subprocess.CalledProcessError as e:
             print(f"{RED}Unsuccessful{RESET}")
+
+    if(choice == '4'):
+
+        try:
+            partition_no = input("partition no: ")
+            user_name = input("Username: ")
+            print(f"Where to mount:\n1>/media/{user_name}\n2>/mnt")
+            ch = input("choose: ")
+            if(ch=='1'):
+                loc = f"/media/{user_name}"
+            else:
+                loc = "/mnt"
+            output = subprocess.run(["sudo","mount",f"/dev/sda{partition_no}",f"{loc}"])
+            if output.returncode==0:
+                print(f"{GREEN}Successfuly mounted{RESET}")
+            else:
+                print(f"{GREEN}Unsuccessful{RESET}")
+
+        except subprocess.CalledProcessError as e:
+            print(f"{RED}Unsuccessful{RESET}")
+            
 
 def main():
     listing()
