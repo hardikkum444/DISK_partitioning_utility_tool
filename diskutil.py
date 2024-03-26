8fbfafef33b1026677efe39b897be12007f8a824
man44 <114590291+hardikkum444@users.noreply.github.com>
2024-03-26 16:59:10 +0530

    mounting functionality added

--- diskutil.py
+++ diskutil.py
@@ -29,15 +29,22 @@ def what():
     if(choice == "1"):
         print("make sure you have unmounted your disk first by chosing option 3")
         
-        commands = "g\nn\n1\n\n\n\nY\nw"
-
-        sudo = getpass.getpass("please enter your root pass")
+        sudo = getpass.getpass("please enter your root pass: ")
+        
+        part_no = input("partition number: ")
+        commands = f"g\nn\n{part_no}\n\n\n\nY\nw"
+        
         subprocess.run(["sudo", "fdisk", "/dev/sda"], input =f"{sudo}\n{commands}", stderr = subprocess.PIPE, text=True, check=True)
-        print("completed")
-        # output = subprocess.check_output(sudo).decode("utf-8")
-        # print(output)
+        
+        print("new partition created")
 
-        subprocess.run(["sudo","mkfs.exfat","-n","pen","/dev/sda1"])
+        name = input("new name: ");
+        subprocess.run(["sudo","mkfs.exfat","-n",name,f"/dev/sda{part_no}"])
+        print("format completed, ready to go")
+        print("now mounting to /mnt")
+
+        subprocess.run(["sudo","mount",f"/dev/sda{part_no}","/media/man44"])
+        print("disk has been mounted")
 
 
 def main():
